4f3b56d3baebd00490edc29c91c8487b149ee2bf
egorovdj <66457899+egorovdj@users.noreply.github.com>
2022-08-14 03:11:41 +0300

    Add files via upload

---
+++ Data/OrdersSimulator.cs
@@ -0,0 +1,11 @@
+namespace Aggregator.Data
+{
+    public class OrdersSimulator
+    {
+        public int Id { get; set; }
+        public string From { get; set; } = String.Empty;
+        public string FromYandex { get; set; } = String.Empty;
+        public string To { get; set; } = String.Empty;
+        public string ToYandex { get; set; } = String.Empty;
+    }
+}
---
+++ Data/TypesOfAggregatorCars.cs
@@ -0,0 +1,23 @@
+﻿namespace Aggregator.Data
+{
+    public class TypesOfAggregatorCars
+    {
+        static readonly Dictionary<string, List<string>> typesOfCars = new()
+        {
+            {"Яндекс.Такси", new List<string> { "Эконом", "Комфорт", "Бизнес", "Премиум" } },
+            {"Uber", new List<string> { "UberX", "UberSelect", "UberBlack" } },
+            {"Ситимобил", new List<string> { "эконом", "комфорт", "бизнес" } },
+            {"Максим", new List<string> { "Эконом", "Комфорт", "Бизнес" } }
+        };
+
+        public static Task<string[]> GetAggregatorAsync()
+        {
+            return Task.FromResult(typesOfCars.Keys.ToArray());
+        }
+
+        public static Task<string[]> GetTypesOfCarsAsync(string aggr)
+        {
+            return Task.FromResult(typesOfCars[aggr].ToArray());
+        }
+    }
+}
